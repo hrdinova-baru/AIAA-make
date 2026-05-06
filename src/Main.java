19607084b4a462e4f3020cc9c7bd6932dea83c46
barahrdinova <hrdinova.baru@gmail.com>
2026-05-06 12:16:57 +0200

    Změna výpisu textu z "Ahoj!" na "čau!"

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    IO.println("Ahoj!");
+    IO.println("čau!");
 
     for (int i = 1; i <= 5; i++) {
         IO.println("i = " + i);
